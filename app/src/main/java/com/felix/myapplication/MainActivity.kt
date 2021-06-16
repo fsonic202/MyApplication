1b59d7ccbd87c4388af1d9a0bf5096d09f5a4d7d
fsonic202 <f.oseh@aol.com>
2021-06-16 18:00:39 +0100

    have added new file to the main file

--- app/src/main/java/com/felix/myapplication/MainActivity.kt
+++ app/src/main/java/com/felix/myapplication/MainActivity.kt
@@ -4,8 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val TAG = "MAIN"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "On start called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "On resume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "On pause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "On stop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "On destroy called")
+    }
+
 }
